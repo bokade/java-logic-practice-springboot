9d28cc4b3d4576c3b59f0b0417afa09d180a3dfc
Swapnil Bokade <sbokade181@gmail.com>
2025-06-17 15:40:20 +0530

    new day 3 program added

---
+++ src/main/java/JavaLogicBuilding/logic/NumberPyramid.java
@@ -0,0 +1,13 @@
+package JavaLogicBuilding.logic;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int rows = 3;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
