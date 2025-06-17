f6a05cc08d7695513830f56832e1d9a372539676
Swapnil Bokade <sbokade181@gmail.com>
2025-06-17 15:47:48 +0530

    changescount

--- src/main/java/JavaLogicBuilding/logic/InvertedPyramid.java
+++ src/main/java/JavaLogicBuilding/logic/InvertedPyramid.java
@@ -2,7 +2,7 @@ package JavaLogicBuilding.logic;
 
 public class InvertedPyramid {
     public static void main(String[] args) {
-        int rows = 3;
+        int rows = 5;
         for (int i = rows; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
--- src/main/java/JavaLogicBuilding/logic/NumberPyramid.java
+++ src/main/java/JavaLogicBuilding/logic/NumberPyramid.java
@@ -2,7 +2,7 @@ package JavaLogicBuilding.logic;
 
 public class NumberPyramid {
     public static void main(String[] args) {
-        int rows = 3;
+        int rows = 5;
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
