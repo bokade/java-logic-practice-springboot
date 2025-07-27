c8c0991a29e149580cbdb90427aa583da4e95bb7
Swapnil Bokade <sbokade181@gmail.com>
2025-07-27 12:03:31 +0530

    java array sort

--- src/main/java/JavaLogicBuilding/logic/CheckSortedArray.java
+++ src/main/java/JavaLogicBuilding/logic/CheckSortedArray.java
@@ -1,18 +1,28 @@
 package JavaLogicBuilding.logic;
 
 public class CheckSortedArray {
+
+    public static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1])
+                return false;
+        }
+        return true;
+    }
+    public static boolean isSortedDescending(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1])
+                return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        boolean isSorted = true;
 
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[i-1]) {
-                isSorted = false;
-                break;
-            }
-        }
-
-        System.out.println("Is array sorted? " + isSorted);
+        System.out.println("Is array sorted? " + isSorted(arr));
+        System.out.println("Is array sorted descending? " + isSortedDescending(arr));
     }
+
 }
 
