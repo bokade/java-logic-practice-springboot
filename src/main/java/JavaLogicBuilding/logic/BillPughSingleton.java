91ec55110d7f4e937aea58e8ece90d6757c418b0
Swapnil Bokade <sbokade181@gmail.com>
2025-08-17 13:05:48 +0530

    added java bill singleton

---
+++ src/main/java/JavaLogicBuilding/logic/BillPughSingleton.java
@@ -0,0 +1,15 @@
+package JavaLogicBuilding.logic;
+
+class BillPughSingleton {
+
+    private BillPughSingleton() {}
+
+    // static inner helper class
+    private static class Helper {
+        private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+    }
+
+    public static BillPughSingleton getInstance() {
+        return Helper.INSTANCE;
+    }
+}
