bfdeceb37ef94dfcf66d9a101cbe8b06c8686ea7
Swapnil Bokade <sbokade181@gmail.com>
2025-08-17 12:54:12 +0530

    added Eager singleton java

---
+++ src/main/java/JavaLogicBuilding/logic/EagerSingleton.java
@@ -0,0 +1,15 @@
+package JavaLogicBuilding.logic;
+
+class EagerSingleton {
+    // object is created at class loading time
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    // private constructor so that no other object can be created
+    private EagerSingleton() {
+    }
+
+    // global access point
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
