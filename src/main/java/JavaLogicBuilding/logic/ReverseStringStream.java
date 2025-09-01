53053fe48aeeb77315bc7d4e6313e580972f0a36
Swapnil Bokade <sbokade181@gmail.com>
2025-09-01 09:45:03 +0530

    modified reverse string using scanner dynamic input

--- src/main/java/JavaLogicBuilding/logic/ReverseStringStream.java
+++ src/main/java/JavaLogicBuilding/logic/ReverseStringStream.java
@@ -1,12 +1,22 @@
 package JavaLogicBuilding.logic;
 
+import java.util.Scanner;
+
 public class ReverseStringStream {
 
     public static void main(String[] args) {
-        String str = "HelloWorld";
-        String reversed = str.chars().mapToObj(c -> (char) c).reduce("", (s, c) -> c + s, (s1, s2) -> s2 + s1);
+        Scanner sc = new Scanner(System.in);   // Scanner object banaya
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();            // User se pura line input liya
+
+        String reversed = str.chars()
+                .mapToObj(c -> (char) c)
+                .reduce("", (s, c) -> c + s, (s1, s2) -> s2 + s1);
+
         System.out.println("Original: " + str);
         System.out.println("Reversed: " + reversed);
-    }
 
+        sc.close();  // Scanner ko band karna best practice hai
+    }
 }
+
