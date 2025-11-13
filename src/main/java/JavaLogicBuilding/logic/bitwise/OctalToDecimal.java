2dde4ec9693d234dad5baff3677457bb15ae8b19
Swapnil Bokade <sbokade181@gmail.com>
2025-11-13 07:14:34 +0530

    added octal to decimal conversion java program

---
+++ src/main/java/JavaLogicBuilding/logic/bitwise/OctalToDecimal.java
@@ -0,0 +1,17 @@
+package JavaLogicBuilding.logic.bitwise;
+
+class OctalToDecimal {
+    public static void main(String[] args) {
+        String octal = "32";
+        int result = 0;
+
+        for (int i = 0; i < octal.length(); i++) {
+            char c = octal.charAt(octal.length() - 1 - i);
+            int digit = c - '0';
+            result += digit * Math.pow(8, i);
+        }
+
+        System.out.println("Decimal: " + result);
+    }
+}
+
